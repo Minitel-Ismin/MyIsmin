5c6ba40be46d7881832411176ca9f78396d8ee51
brutia <guillaume.andres@yahoo.fr>
2016-06-09 14:20:08 +0200

    Https support ==> mettre APP_ENV=server dans .env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Assos;
-use App\Clubs;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+    	if (!\App::environment('local')) {
+    		\URL::forceSchema('https');
+    	}
     }
 
     /**
--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title')</title>
     
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <link href="{{ URL::asset('assets/css/main.css' )}}" rel="stylesheet">
     <link href="{{ URL::asset('assets/css/clean-blog.css' )}}" rel="stylesheet">
